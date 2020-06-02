f027ba0adef429b15ff554e138092f023a6b207d
Octavio E de Lima <odelima@uoregon.edu>
2020-06-02 18:29:12 -0300

    Changes to Servers

---
+++ client/javascripts/todo.js
@@ -0,0 +1,108 @@
+//jshint esversion: 6
+
+let controller = function() {
+  //load comments from db when page loads
+  //if (localStorage.getItem("commentsList")) {
+  //  $(".comments").html(localStorage.getItem("commentsList"));
+  //}
+
+  $.ajax({
+    url: "http://localhost:8888/todos",
+    method: "GET"
+  }).done((res) => {
+    let pElem;
+    //console.log(res.comments[0]._id + " " + res.comments[0].data)
+    res.todos.forEach((todo) => {
+      pElem = $("<p>").html(todo.data)
+      $(".todos").append(pElem);
+    })
+  });
+
+  let addTodoFromInputBox = function() {
+
+    let $new_todo, content;
+
+    if ($(".todo-input input").val() !== "") {
+      content = $(".todo-input input").val();
+      $new_todo = $("<p>").text(content);
+
+      $(".todos").append($new_todo);
+
+      $(".todo-input input").val("");
+
+      //add todo to db
+      $.ajax({
+          method: "POST",
+          url: "http://localhost:8888/addtodo",
+          data: {
+            data: content
+          }
+        })
+        .done(function(msg) {
+          console.log("Data Saved: " + msg);
+        });
+    }
+  };
+
+  $(".todo-input button").on("click", function(event) {
+    addTodoFromInputBox();
+  });
+
+  $(".todo-input input").on("keypress", function(event) {
+    if (event.keyCode === 13) {
+      addTodoFromInputBox();
+    }
+  });
+};
+
+let deleteTodo = () => {
+  //delete a todo from db
+  let content = $("#deleteOne").val();
+  $.ajax({
+      method: "POST",
+      url: "http://localhost:8888/deletetodo/" + content
+    })
+    .done(function(msg) {
+      console.log("Todo deleted: " + msg);
+    });
+
+  window.location.reload();
+}
+
+let getTodo = () => {
+  //clear outDiv
+  $("#outDiv").html("");
+  let pElem;
+  //retrieve a todo from db
+  let content = $("#getOne").val();
+  $.ajax({
+      method: "GET",
+      url: "http://localhost:8888/gettodo/" + content
+    })
+    .done(function(msg) {
+      console.log("Todo retrieved: " + msg.message.data);
+      pElem = $("<p>").html("Todo Retrieved: " + msg.message.data)
+      $("#outDiv").append(pElem);
+    });
+
+  //window.location.reload();
+}
+
+let deleteAll = () => {
+  //delete all comments from db
+//  localStorage.removeItem("commentsList")
+  window.location.reload();
+}
+
+$(document).ready(() => {
+  let btn01, btn02, btn03;
+  //console.log("ready")
+  //select the delete button
+  btn03 = document.querySelectorAll('button')[3];
+  btn03.addEventListener('click', deleteAll);
+  btn02 = document.querySelectorAll('button')[2];
+  btn02.addEventListener('click', deleteTodo);
+  btn01 = document.querySelectorAll('button')[1];
+  btn01.addEventListener('click', getTodo);
+  controller();
+});
